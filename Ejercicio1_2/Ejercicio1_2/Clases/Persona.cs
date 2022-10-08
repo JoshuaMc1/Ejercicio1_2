7294516fd19f92fa1bf414b84aeaff1d2bd28f5d
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2022-10-08 17:27:24 -0500

    Initial commit

---
+++ Ejercicio1_2/Ejercicio1_2/Clases/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_2.Clases
+{
+    public class Persona
+    {
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public int edad { get; set; }
+        public string correo { get; set; }
+
+        public Persona (string nombres, string apellidos, int edad, string correo)
+        {
+            this.nombres = nombres;
+            this.apellidos = apellidos;
+            this.edad = edad;
+            this.correo = correo;
+        }
+    }
+}
---
+++ Ejercicio1_2/Ejercicio1_2/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public bool validarCampoVacio(string campo)
+        {
+            return String.IsNullOrEmpty(campo);
+        }
+
+        private async void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+            if (!validarCampoVacio(txtNombres.Text) && !validarCampoVacio(txtApellidos.Text) && !validarCampoVacio(txtCorreo.Text) && !validarCampoVacio(txtEdad.Text))
+            {
+                var datos = new Clases.Persona(
+                    nombres: txtNombres.Text,
+                    apellidos: txtApellidos.Text,
+                    edad: int.Parse(txtEdad.Text),
+                    correo: txtCorreo.Text);
+                var siguiente = new Vistas.Informacion();
+                siguiente.BindingContext = datos;
+                await Navigation.PushAsync(siguiente);
+            }
+            else await DisplayAlert("Campos vacios", "Al parecer hay uno o mas campos vacios, debe ingresar todos los datos solicitados.", "Ok");
+        }
+    }
+}
